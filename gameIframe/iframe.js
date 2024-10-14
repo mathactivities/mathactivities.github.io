58a488cad63dea3aa097e35e2d2cd679efba2579
uniqueflabbergast <krupalt78@gmail.com>
2024-10-14 15:32:18 -0700

    Update iframe.js

--- gameIframe/iframe.js
+++ gameIframe/iframe.js
@@ -2,16 +2,16 @@ var fragmentIdentifier = window.location.hash.slice(1)
 var games = ['rocket-league', 'subway-surfers', 'snow-rider', 'gun-spin']
 var iframe = document.createElement('iframe');
 
-iframe.src = "https://" + window.location.hostname + '/games/' + fragmentIdentifier
+iframe.src = "../games/"+ fragmentIdentifier
 for (var i = 0; i < games.length; i++){
     if (fragmentIdentifier == games[i]){
-        iframe.src = 'https://' + window.location.hostname + '/' + fragmentIdentifier
+        iframe.src = '../' + fragmentIdentifier
     }
 }
 
 iframe.style.border='none';
 iframe.style.width = '100%' ;
-iframe.style.height = "100vh"
+iframe.style.height = "100vw"
 iframe.style.margin = '0';
 
 document.body.appendChild(iframe);
@@ -20,5 +20,5 @@ iframe.focus()
 
 var back = document.getElementById('return')
 back.addEventListener('click', ()=>{
-    window.location.href = "https://" + window.location.hostname
+    window.location.href = '../index.html'
 })
