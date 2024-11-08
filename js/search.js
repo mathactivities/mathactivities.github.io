5f410462bbccd512bff1780a62b4e040d219102b
mathactivities <krupalt78@gmail.com>
2024-11-07 17:27:46 -0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -6,13 +6,12 @@ console.log(items.length)
 document.getElementById("no-of-games").textContent = items.length + " GAMES"
 const genres = {
     'hot': Array.from(itemList.getElementsByClassName('hot')),
-    'shooting': Array.from(itemList.getElementsByClassName('shooting')),
+    'shoot': Array.from(itemList.getElementsByClassName('shooting')),
     'car': Array.from(itemList.getElementsByClassName('car')),
     'multiplayer': Array.from(itemList.getElementsByClassName('multiplayer')),
     'singleplayer': Array.from(itemList.getElementsByClassName('singleplayer')),
     'flash': Array.from(itemList.getElementsByClassName('retro')),
     'parkour': Array.from(itemList.getElementsByClassName('parkour')),
-    'sports': Array.from(itemList.getElementsByClassName('sport')),
     'sport': Array.from(itemList.getElementsByClassName('sport')),
 }
 const genreKeys = Object.keys(genres)
@@ -21,9 +20,16 @@ searchInput.addEventListener('input', function() {
     for (var i = 0; i < genreKeys.length; i++) {
         if (searchTerm.startsWith("/cat ") && genreKeys[i] == searchTerm.substring(5)) {
             items.forEach(item => {
-                item.style.display = item.classList.contains(genreKeys[i]) ? 'block' : 'none';
+                if (item.classList.contains(genreKeys[i])){
+                    item.classList.remove('hide')
+                }
+                else {
+                    item.classList.add('hide')
+                }
             });
-        } else if (!searchTerm.startsWith("/cat ")) {
+        } 
+        else 
+        if (!searchTerm.startsWith("/cat ")) {
             items.forEach(function(item) {
                 const itemName = item.textContent.toLowerCase();
                 if (itemName.includes(searchTerm)) {
@@ -32,6 +38,7 @@ searchInput.addEventListener('input', function() {
                     item.classList.add('hide');
                 }
             });
-        }
+        } 
     }
+    
 });
