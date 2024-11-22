b407066ac48ad36a3e2811de1f672f15f6281637
computgeeks <krupalt78@gmail.com>
2024-11-21 16:27:23 -0800

    Add Several Games, Add loading Screen

--- js/loading.js
+++ js/loading.js
@@ -1,4 +1,5 @@
-const loading = document.getElementById('load')
+document.body.innerHTML = `<div id = "load" style="text-align:center; padding-top: 100px; font-size: 60px;  position: absolute; top: 0; z-index: 10000000;visibility: visible;  width: 100%; height: 100vh;display: block; color: rgb(255, 255, 255); background-color: rgb(18, 18, 18); position: absolute; z-index: 2147483647;">Loading...</div>` + document.body.innerHTML
+const loading = document.getElementById("load")
 window.addEventListener('load', () => {
-    loading.style.display = "none"
+    loading.style.visibility = "hidden"
 })
