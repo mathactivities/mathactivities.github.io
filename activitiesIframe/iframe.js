4ce7d41fdc19a11afb1925930d67a01b4b4bd9f1
krupaltisgaonkar <krupalt78@gmail.com>
2025-04-19 20:58:57 -0700

    eee

--- activitiesIframe/iframe.js
+++ activitiesIframe/iframe.js
@@ -93,7 +93,7 @@ var flash = ['escape-the-closet'    ,
     'escape-the-car' ,            
    'riddle-transfer-2']
 
-const other = ["time2", "gladis", "tetrs", "boxing-random", "polytrack", "superhot", "escaperoadcity2"]
+const other = ["time2", "gladis", "tetrs", "boxing-random", "polytrack", "superhot", "escaperoadcity2", "escaperoadcity", "smashyroad"]
 var iframe = document.createElement('iframe');
 
 iframe.src = "../activities/"+ fragmentIdentifier
